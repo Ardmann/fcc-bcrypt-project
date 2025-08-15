737d31ce2026132d9b372a2ee13527aa06abb7be
Ardmann <armando.orellana03@gmail.com>
2025-08-15 03:32:09 -0400

    server.js

--- boilerplate-bcrypt/server.js
+++ boilerplate-bcrypt/server.js
@@ -1,24 +1,42 @@
-"use strict";
-const express = require("express");
-const bodyParser = require("body-parser");
-const fccTesting = require("./freeCodeCamp/fcctesting.js");
-const app = express();
+'use strict';
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const bcrypt      = require('bcrypt'); // ✅ bcrypt nativo
+const app         = express();
+
 fccTesting(app);
+
 const saltRounds = 12;
-const myPlaintextPassword = "sUperpassw0rd!";
-const someOtherPlaintextPassword = "pass123";
+const myPlaintextPassword = 'sUperpassw0rd!';
+const someOtherPlaintextPassword = 'pass123';
 
-const bcrypt = require("bcrypt");
+//START_ASYNC
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  if (err) throw err;
+  console.log('Async hash:', hash);
 
-//START_ASYNC -do not remove notes, place code between correct pair of notes.
+  bcrypt.compare(myPlaintextPassword, hash, (err, result) => {
+    if (err) throw err;
+    console.log('Async password match:', result);
+  });
 
+  bcrypt.compare(someOtherPlaintextPassword, hash, (err, result) => {
+    if (err) throw err;
+    console.log('Async wrong password match:', result);
+  });
+});
 //END_ASYNC
 
 //START_SYNC
+const hashSync = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log('Sync hash:', hashSync);
 
+console.log('Sync password match:', bcrypt.compareSync(myPlaintextPassword, hashSync));
+console.log('Sync wrong password match:', bcrypt.compareSync(someOtherPlaintextPassword, hashSync));
 //END_SYNC
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("Listening on port:", PORT);
+  console.log("Listening on port:", PORT);
 });
